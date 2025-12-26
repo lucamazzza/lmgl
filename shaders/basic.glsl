c1c009b08ffbb96b727def486967fbee75518e0a
Luca Mazza <luca.mazza@mazluc.ch>
2025-12-26 17:53:52 +0100

    chore: base shaders (both .glsl and .vert/.frag)

---
+++ shaders/basic.glsl
@@ -0,0 +1,25 @@
+#shader vertex
+#version 410 core
+
+layout(location = 0) in vec3 a_Position;
+layout(location = 1) in vec4 a_Color;
+
+uniform mat4 u_ViewProjection;
+uniform mat4 u_Transform;
+
+out vec4 v_Color;
+
+void main() {
+    v_Color = a_Color;
+    gl_Position = u_ViewProjection * u_Transform * vec4(a_Position, 1.0);
+}
+
+#shader fragment
+#version 410 core
+
+in vec4 v_Color;
+out vec4 FragColor;
+
+void main() {
+    FragColor = v_Color;
+}
