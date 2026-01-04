64d3b944113aaa30cc8705486cc2a553c49876fc
Luca Mazza <luca.mazza@mazluc.ch>
2026-01-04 12:59:18 +0100

    chore: updated shaders to match renderer

--- shaders/basic.glsl
+++ shaders/basic.glsl
@@ -2,16 +2,26 @@
 #version 410 core
 
 layout(location = 0) in vec3 a_Position;
-layout(location = 1) in vec4 a_Color;
+layout(location = 1) in vec3 a_Normal;
+layout(location = 2) in vec4 a_Color;
+layout(location = 3) in vec2 a_TexCoord;
 
-uniform mat4 u_ViewProjection;
-uniform mat4 u_Transform;
+uniform mat4 u_Model;
+uniform mat4 u_View;
+uniform mat4 u_Projection;
+uniform mat4 u_NormalMatrix;
 
+out vec3 v_FragPos;
+out vec3 v_Normal;
 out vec4 v_Color;
+out vec2 v_TexCoord;
 
 void main() {
+    v_FragPos = vec3(u_Model * vec4(a_Position, 1.0));
+    v_Normal = u_NormalMatrix * a_Normal;
     v_Color = a_Color;
-    gl_Position = u_ViewProjection * u_Transform * vec4(a_Position, 1.0);
+    v_TexCoord = a_TexCoord;
+    gl_Position = u_Projection * u_View * u_Model * vec4(a_Position, 1.0);
 }
 
 #shader fragment
